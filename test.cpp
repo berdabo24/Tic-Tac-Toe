434a6f5f64d04186781a008185342d12a3d4687e
berdabo24 <juliankoh2023@gmail.com>
2024-12-19 14:28:17 +0800

    CleanState

--- test.cpp
+++ test.cpp
@@ -3,9 +3,5 @@ using namespace std;
 
 int main(){
     cout << "Hello World" << endl;
-    cout << "Hllllllllllo" << endl;
-    cout << "yes" << endl;
-    cout << "skibidi la" << endl;
-    cout << "Papadum" << endl;
     return 0;
 }
